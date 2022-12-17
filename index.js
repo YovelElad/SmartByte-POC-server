6d31d8b26080ee9fc78aa03356d94972ce78d8e5
aavidan <amir.avidan@beyondminds.ai>
2022-12-17 14:47:59 +0200

    Added function for calling python script

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 const fs = require('fs');
 const readline = require('readline');
-  
+
 
 const file = readline.createInterface({
     input: fs.createReadStream('../SmartByte-Interpreter/functions.txt'),
@@ -34,55 +34,79 @@ file.on('line', (line) => {
 
 function TrunOnTheAc() {
     console.log("Turn on the ac");
-    //     try {
-    //     const response = await axios({
-    //         url: `${process.env.SENSOR_URL}/temperature`,
-    //         method: "get",
-    //     });
-    //     // res.status(200).json(response.data);
-    //     if(response.data>=20){
-    //         console.log("Activte AC");
-    //         // setACState();
-    //     }
-    // } catch (err) {
-    //     // res.status(500).json({ message: err });
-    //     console.log(err + "Cant read from esp32's sensor");
-    // };
+        try {
+        const response = await axios({
+            url: `${process.env.SENSOR_URL}/temperature`,
+            method: "get",
+        });
+        // res.status(200).json(response.data);
+            console.log("Activte AC");
+            // setACState();
+    } catch (err) {
+        // res.status(500).json({ message: err });
+        console.log(err + "Cant read from esp32's sensor");
+    };
 }
 
 
-// setInterval(async()=>{
-//     try {
-//     const response = await axios({
-//         url: `${process.env.SENSOR_URL}/temperature`,
-//         method: "get",
-//     });
-//     // res.status(200).json(response.data);
-//     if(response.data>=20){
-//         setACState();
-//     }
-// } catch (err) {
-//     // res.status(500).json({ message: err });
-//     console.log(err + "Cant read from esp32's sensor");
-// }},8000);
+setInterval(async()=>{
+    try {
+    const response = await axios({
+        url: `${process.env.SENSOR_URL}/temperature`,
+        method: "get",
+    });
+    // res.status(200).json(response.data);
+    if(response.data>=20){
+        setACState();
+    }
+} catch (err) {
+    // res.status(500).json({ message: err });
+    console.log(err + "Cant read from esp32's sensor");
+}},8000);
 
-// app.get('/temperature',setInterval(async(req, res) => {
-// 	try {
-//         const response = await axios({
-//             url:`${process.env.SENSOR_URL}/temperature`,
-// 			method: "get",
-// 		});
-// 		res.status(200).json(response.data);
-//         if(response.data>=20){
-//             setACState();
-//         }
-// 	} catch (err) {
-// 		res.status(500).json({ message: err });
-// 	}
-// },8000));
+app.get('/temperature',setInterval(async(req, res) => {
+	try {
+        const response = await axios({
+            url:`${process.env.SENSOR_URL}/temperature`,
+			method: "get",
+		});
+		res.status(200).json(response.data);
+        if(response.data>=20){
+            setACState();
+        }
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+},8000));
 
 
 app.all('*', (req, res) => res.send('Global handler'));
 
 app.listen(port);
 console.log('listening to port', port)
+
+/**
+ * @param {string} tempVal The string
+ */
+function insertValueOfTempSensor(tempVal) {
+    const PythonShell = require('python-shell').PythonShell;
+
+    var options = {
+        mode: 'text',
+        pythonPath: '/usr/bin/python3',
+        pythonOptions: ['-u'],
+        scriptPath: '/Users/amiravidan/Documents/finalProject/SmartByte-Interpreter',
+        args: ['temperature', tempVal]
+    };
+
+    process.chdir('../SmartByte-Interpreter');
+
+    PythonShell.run('setValueBySensor.py', options, function (err, results) {
+        if (err)
+            throw err;
+        console.log('Changed temp value');
+    });
+
+    process.chdir('../SmartByte-POC-server')
+
+}
